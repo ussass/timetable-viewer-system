4affeef8e0b2b5a522199177ed1f5fb4b9a8dea4
ussass <ussass113@gmail.com>
2021-06-29 17:24:35 +0300

    fix create table

--- src/main/resources/db/migration/V1__create_tables.sql
+++ src/main/resources/db/migration/V1__create_tables.sql
@@ -1,17 +1,17 @@
 CREATE TABLE IF NOT EXISTS students(
-    student_id SERIAL NOT NULL CONSTRAINT students_pkey PRIMARY KEY,
+    student_id SERIAL NOT NULL PRIMARY KEY,
     group_id INTEGER NOT NULL,
     first_name VARCHAR(255) NOT NULL,
     last_name VARCHAR(255) NOT NULL
     );
 
 CREATE TABLE IF NOT EXISTS teachers(
-    teacher_id SERIAL NOT NULL CONSTRAINT students_pkey PRIMARY KEY,
+    teacher_id SERIAL NOT NULL PRIMARY KEY,
     first_name VARCHAR(255) NOT NULL,
     last_name VARCHAR(255) NOT NULL
     );
 
 CREATE TABLE IF NOT EXISTS groups(
-    group_id SERIAL NOT NULL CONSTRAINT groups_pkey PRIMARY KEY,
-    group_name VARCHAR(255) NOT NULL,
+    group_id SERIAL NOT NULL PRIMARY KEY,
+    group_name VARCHAR(255) NOT NULL
     );
