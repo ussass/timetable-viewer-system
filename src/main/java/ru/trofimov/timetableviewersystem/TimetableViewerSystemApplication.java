65eb963a34cb22d138f65035d9df1ea0061ee358
ussass <ussass113@gmail.com>
2021-06-29 16:40:22 +0300

    added spring

---
+++ src/main/java/ru/trofimov/timetableviewersystem/TimetableViewerSystemApplication.java
@@ -0,0 +1,14 @@
+package ru.trofimov.timetableviewersystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimetableViewerSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TimetableViewerSystemApplication.class, args);
+		System.out.println(11111);
+	}
+
+}
