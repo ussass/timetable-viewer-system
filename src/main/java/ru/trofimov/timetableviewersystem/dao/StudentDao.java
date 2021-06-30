e1f1cd55655e4f01e2365b06de1f9783181727f4
ussass <ussass113@gmail.com>
2021-06-30 17:24:47 +0300

    added StudentDao

--- src/main/java/ru/trofimov/timetableviewersystem/dao/StudentDao.java
+++ src/main/java/ru/trofimov/timetableviewersystem/dao/StudentDao.java
@@ -1,4 +1,11 @@
 package ru.trofimov.timetableviewersystem.dao;
 
+import ru.trofimov.timetableviewersystem.model.Student;
+
+import java.util.List;
+
 public interface StudentDao {
+    void add(Student student);
+    public List<Student> findAll();
+    public Student findStudentById(int id);
 }
