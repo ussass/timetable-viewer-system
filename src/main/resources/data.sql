b24fc8d97374854d59ff65ebf0cda1ab865d1ecb
ussass <ussass113@gmail.com>
2021-06-30 13:19:28 +0300

    removed unnecessary

--- src/main/resources/data.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO tbl_employees(first_name, last_name, email) VALUES
-    ('Dan', 'Vega', 'danvega@gmail.com'),
-    ('Jen', 'Vega', 'jenvega@email.com'),
-    ('Bella', 'Vega', 'bellavega@gmail.com');
--- src/main/resources/schema.sql
+++
@@ -1,8 +0,0 @@
-DROP TABLE IF EXISTS tbl_employees;
-
-CREATE TABLE tbl_employees(
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    first_name VARCHAR(250) NOT NULL,
-    last_name VARCHAR(250) NOT NULL,
-    email VARCHAR(250) DEFAULT NULL
-);
