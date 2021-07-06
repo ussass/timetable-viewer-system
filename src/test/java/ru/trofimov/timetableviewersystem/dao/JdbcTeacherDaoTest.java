ad32728e0319b290367965861d68a45a7c6de378
mikle <mikletrof@gmail.com>
2021-07-06 18:56:59 +0300

    added teacher dao test

--- src/test/java/ru/trofimov/timetableviewersystem/dao/JdbcTeacherDaoTest.java
+++ src/test/java/ru/trofimov/timetableviewersystem/dao/JdbcTeacherDaoTest.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import ru.trofimov.timetableviewersystem.model.Student;
+import ru.trofimov.timetableviewersystem.model.Teacher;
 
 import java.sql.SQLException;
 import java.util.List;
@@ -21,47 +21,47 @@ class JdbcTeacherDaoTest {
     @Test
     @Order(1)
     void shouldFindAll() {
-        List<Student> students = studentDao.findAll();
-        students.forEach(System.out::println);
-        assertEquals(2, students.size());
+        List<Teacher> teachers = teacherDao.findAll();
+        teachers.forEach(System.out::println);
+        assertEquals(2, teachers.size());
     }
 
     @Test
     @Order(2)
     void shouldFindById() {
-        Student student = studentDao.findById(1L);
-        assertEquals("Student{studentId=1, groupId=1, Tony Stark}", student.toString());
+        Teacher teacher = teacherDao.findById(1L);
+        assertEquals("Teacher{id=1, Jacob Smith}", teacher.toString());
     }
 
     @Test
     @Order(3)
     void shouldAdd() throws SQLException {
-        Student student = new Student("fName", "lName");
-        student = studentDao.save(student);
-        System.out.println("student.getId() = " + student.getId());
-        assertNotEquals(0, (long) student.getId());
+        Teacher teacher = new Teacher("fName", "lName");
+        teacher = teacherDao.save(teacher);
+        System.out.println("student.getId() = " + teacher.getId());
+        assertNotEquals(0, (long) teacher.getId());
     }
 
     @Test
     @Order(4)
     void shouldUpdate() throws SQLException {
-        Student student = new Student("newFirst", "newLast");
-        List<Student> students = studentDao.findAll();
-        long count = students.get(students.size() - 1).getId();
-        student.setId(count);
+        Teacher teacher = new Teacher("newFirst", "newLast");
+        List<Teacher> teachers = teacherDao.findAll();
+        long count = teachers.get(teachers.size() - 1).getId();
+        teacher.setId(count);
         System.out.println("count = " + count);
-        studentDao.update(student);
-        Student studentExpected = studentDao.findById(count);
-        assertEquals("Student{studentId=2, groupId=0, newFirst newLast}", studentExpected.toString());
+        teacherDao.update(teacher);
+        Teacher teacherExpected = teacherDao.findById(count);
+        assertEquals("Teacher{id=2, newFirst newLast}", teacherExpected.toString());
     }
 
     @Test
     @Order(5)
     void shouldDelete() throws SQLException {
-        List<Student> students = studentDao.findAll();
-        long count = students.get(students.size() - 1).getId();
-        studentDao.delete(count);
-        List<Student> studentsExpected = studentDao.findAll();
-        assertEquals(2, studentsExpected.size());
+        List<Teacher> teachers = teacherDao.findAll();
+        long count = teachers.get(teachers.size() - 1).getId();
+        teacherDao.delete(count);
+        List<Teacher> teachersExpected = teacherDao.findAll();
+        assertEquals(2, teachersExpected.size());
     }
 }
