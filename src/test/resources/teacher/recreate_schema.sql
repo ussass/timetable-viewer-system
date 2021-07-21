9260e8e848cf41c7154ef272a6852eb3bc244f2d
ussass <ussass113@gmail.com>
2021-07-21 14:56:43 +0300

    updated tests

--- src/test/resources/teacher/recreate_schema.sql
+++ src/test/resources/teacher/recreate_schema.sql
@@ -1,7 +1,7 @@
 DROP TABLE IF EXISTS teachers CASCADE;
 
 CREATE TABLE IF NOT EXISTS teachers(
-       teacher_id SERIAL NOT NULL PRIMARY KEY,
+       teacher_id BIGSERIAL NOT NULL PRIMARY KEY,
        first_name VARCHAR(255) NOT NULL,
        last_name VARCHAR(255) NOT NULL
 );
