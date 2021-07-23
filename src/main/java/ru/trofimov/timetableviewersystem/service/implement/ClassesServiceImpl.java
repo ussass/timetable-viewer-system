b733d3dd9bf64aeac39428460340e85fb02c0384
ussass <ussass113@gmail.com>
2021-07-23 10:06:11 +0300

    added logging

--- src/main/java/ru/trofimov/timetableviewersystem/service/implement/ClassesServiceImpl.java
+++ src/main/java/ru/trofimov/timetableviewersystem/service/implement/ClassesServiceImpl.java
@@ -1,5 +1,7 @@
 package ru.trofimov.timetableviewersystem.service.implement;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import ru.trofimov.timetableviewersystem.dao.ClassesDao;
@@ -13,6 +15,7 @@ import java.util.List;
 public class ClassesServiceImpl implements ClassesService {
 
     private final ClassesDao classesDao;
+    private static final Logger logger = LoggerFactory.getLogger(ClassesServiceImpl.class);
 
     public ClassesServiceImpl(ClassesDao classesDao) {
         this.classesDao = classesDao;
@@ -21,30 +24,35 @@ public class ClassesServiceImpl implements ClassesService {
     @Override
     @Transactional
     public Classes save(Classes entity) throws SQLException {
+        logger.info("saved new classes");
         return classesDao.save(entity);
     }
 
     @Override
     @Transactional(readOnly = true)
     public List<Classes> findAll() {
+        logger.info("Got all classes");
         return classesDao.findAll();
     }
 
     @Override
     @Transactional(readOnly = true)
     public Classes findById(Long id) throws SQLException {
+        logger.info("Got classes by id = {}", id);
         return classesDao.findById(id);
     }
 
     @Override
     @Transactional
     public Classes update(Classes entity) throws SQLException {
+        logger.info("updated classes with id = {}", entity.getId());
         return classesDao.update(entity);
     }
 
     @Override
     @Transactional
     public void delete(Long id) throws SQLException {
+        logger.info("deleted classes with id = {}", id);
         classesDao.delete(id);
     }
 }
