dfb1a518af311a377135e4bdaec7336902e511d9
ussass <ussass113@gmail.com>
2021-07-27 13:03:32 +0300

    added student db show

--- src/main/java/ru/trofimov/timetableviewersystem/controller/TeacherController.java
+++ src/main/java/ru/trofimov/timetableviewersystem/controller/TeacherController.java
@@ -6,6 +6,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import ru.trofimov.timetableviewersystem.service.TeacherService;
 
+import java.sql.SQLException;
+
 @Controller
 @RequestMapping("/teachers")
 public class TeacherController {
@@ -17,6 +19,11 @@ public class TeacherController {
 
     @GetMapping()
     public String showAll(Model model) {
+        try {
+            model.addAttribute("teachers", teacherService.findAll());
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
         return "teachers/index";
     }
 
