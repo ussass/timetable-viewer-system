1aa8bf42d32ee4bc85306cb54b7a9eac205029e8
ussass <ussass113@gmail.com>
2021-07-29 14:09:08 +0300

    Added tests

---
+++ src/test/java/ru/trofimov/timetableviewersystem/controller/MainControllerTest.java
@@ -0,0 +1,31 @@
+package ru.trofimov.timetableviewersystem.controller;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.hamcrest.core.StringContains.containsString;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+@AutoConfigureMockMvc
+class MainControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    void shouldShowIndex() throws Exception {
+        this.mockMvc.perform(get("/"))
+                .andDo(print()).andExpect(status().isOk())
+                .andExpect(content().string(containsString("Timetable viewer system")));
+    }
+}
