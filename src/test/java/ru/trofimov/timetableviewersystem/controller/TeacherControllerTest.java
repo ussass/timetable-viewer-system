2d27d34f2c6c0ccd12b37c07b3dc0d24391f6f9d
Trofimov <mikletrof@gmail.com>
2021-08-02 13:35:35 +0300

    edited test

--- src/test/java/ru/trofimov/timetableviewersystem/controller/TeacherControllerTest.java
+++ src/test/java/ru/trofimov/timetableviewersystem/controller/TeacherControllerTest.java
@@ -3,29 +3,65 @@ package ru.trofimov.timetableviewersystem.controller;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
+import ru.trofimov.timetableviewersystem.dao.jdbc.*;
+import ru.trofimov.timetableviewersystem.model.Teacher;
+import ru.trofimov.timetableviewersystem.service.ClassesService;
+import ru.trofimov.timetableviewersystem.service.TeacherService;
 
+import java.util.Arrays;
+
+import static org.assertj.core.internal.bytebuddy.matcher.ElementMatchers.is;
 import static org.hamcrest.core.StringContains.containsString;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @ExtendWith(SpringExtension.class)
-@SpringBootTest
-@AutoConfigureMockMvc
+@WebMvcTest(TeacherController.class)
 class TeacherControllerTest {
+
+    @MockBean
+    private TeacherService teacherService;
 
     @Autowired
     private MockMvc mockMvc;
 
+    @MockBean
+    private JdbcClassesDao jdbcClassesDao;
+
+    @MockBean
+    private JdbcClassroomDao jdbcClassroomDao;
+
+    @MockBean
+    private JdbcCourseDao jdbcCourseDao;
+
+    @MockBean
+    private JdbcGroupDao jdbcGroupDao;
+
+    @MockBean
+    private JdbcLessonSlotDao jdbcLessonSlotDao;
+
+    @MockBean
+    private JdbcStudentDao jdbcStudentDao;
+
+    @MockBean
+    private JdbcTeacherDao jdbcTeacherDao;
+
     @Test
-    void shouldShowIndex() throws Exception {
-        this.mockMvc.perform(get("/teachers"))
-                .andDo(print()).andExpect(status().isOk())
+    void shouldGetAllGroups() throws Exception {
+        String url = "/teachers";
+        Teacher entity = new Teacher("Test", "test");
+        entity.setId(1L);
+        when(teacherService.findAll()).thenReturn(Arrays.asList(entity));
+        mockMvc.perform(get(url))
+                .andExpect(status().isOk())
+                .andExpect(view().name("teachers/index"))
+                .andExpect(model().attribute("active", "teachers"))
                 .andExpect(content().string(containsString("Teachers List")));
     }
+
 }
