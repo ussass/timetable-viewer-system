7ce9f05b912297b32237d58c3c805e31b40376dc
Trofimov <mikletrof@gmail.com>
2021-08-03 11:41:23 +0300

    add tests

--- src/test/java/ru/trofimov/timetableviewersystem/controller/TeacherControllerTest.java
+++ src/test/java/ru/trofimov/timetableviewersystem/controller/TeacherControllerTest.java
@@ -8,11 +8,12 @@ import org.springframework.test.web.servlet.MockMvc;
 import ru.trofimov.timetableviewersystem.model.Teacher;
 import ru.trofimov.timetableviewersystem.service.TeacherService;
 
+import java.sql.SQLException;
 import java.util.Arrays;
 
 import static org.hamcrest.Matchers.*;
 import static org.hamcrest.core.StringContains.containsString;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
@@ -26,7 +27,7 @@ class TeacherControllerTest {
     private MockMvc mockMvc;
 
     @Test
-    void shouldGetAllGroups() throws Exception {
+    void shouldGetAllTeachers() throws Exception {
         String url = "/teachers";
         Teacher teacher = new Teacher("Test", "test");
         teacher.setId(1L);
@@ -42,6 +43,55 @@ class TeacherControllerTest {
                 .andExpect(content().string(containsString("Teachers List")));
     }
 
+    @Test
+    void shouldGetErrorMessage() throws Exception {
+        String url = "/teachers";
+        when(teacherService.findAll()).thenThrow(new SQLException());
+        mockMvc.perform(get(url))
+                .andExpect(status().isOk())
+                .andExpect(view().name("teachers/index"))
+                .andExpect(model().attribute("active", "teachers"))
+                .andExpect(model().attribute("errorMessage", "Failed to load data"))
+                .andExpect(content().string(containsString("Teachers List")));
+    }
 
+    @Test
+    void shouldGetNewForm() throws Exception {
+        String url = "/teachers/new";
+        mockMvc.perform(get(url))
+                .andExpect(status().isOk())
+                .andExpect(view().name("teachers/new"))
+                .andExpect(model().attribute("active", "teachers"))
+                .andExpect(content().string(containsString("Add new teacher")));
+    }
 
+    @Test
+    void shouldGetUpdateForm() throws Exception {
+        long id = 1L;
+        String url = "/teachers/edit/" + id;
+        Teacher teacher = new Teacher("Test", "test");
+        teacher.setId(id);
+        when(teacherService.findById(id)).thenReturn(teacher);
+        mockMvc.perform(get(url))
+                .andExpect(status().isOk())
+                .andExpect(view().name("teachers/edit"))
+                .andExpect(model().attribute("active", "teachers"))
+                .andExpect(model().attribute("teacher", is(teacher)))
+                .andExpect(content().string(containsString("Edit teacher")));
+    }
+
+    @Test
+    void shouldGetUpdateErrorMessage() throws Exception {
+        long id = 1L;
+        String url = "/teachers/edit/" + id;
+        Teacher teacher = new Teacher("Test", "test");
+        teacher.setId(id);
+        when(teacherService.findById(id)).thenThrow(new SQLException());
+        mockMvc.perform(get(url))
+                .andExpect(status().isOk())
+                .andExpect(view().name("teachers/edit"))
+                .andExpect(model().attribute("active", "teachers"))
+                .andExpect(model().attribute("errorMessage", "Failed to load data"))
+                .andExpect(content().string(containsString("Edit teacher")));
+    }
 }
