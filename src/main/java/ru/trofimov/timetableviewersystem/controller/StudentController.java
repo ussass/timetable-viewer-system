66403f4a5b23af88adf3b57959bd8619fa9517d0
Trofimov <mikletrof@gmail.com>
2021-08-03 14:58:39 +0300

    added new, edit and delete student

--- src/main/java/ru/trofimov/timetableviewersystem/controller/StudentController.java
+++ src/main/java/ru/trofimov/timetableviewersystem/controller/StudentController.java
@@ -2,10 +2,11 @@ package ru.trofimov.timetableviewersystem.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import ru.trofimov.timetableviewersystem.model.Group;
 import ru.trofimov.timetableviewersystem.model.Student;
+import ru.trofimov.timetableviewersystem.model.Teacher;
 import ru.trofimov.timetableviewersystem.service.GroupService;
 import ru.trofimov.timetableviewersystem.service.StudentService;
 
@@ -45,6 +46,83 @@ public class StudentController {
             model.addAttribute("students", studentList);
         }
         return "students/index";
+    }
+
+    @GetMapping("/new")
+    public String newStudent(Model model) {
+        model.addAttribute("active", "students");
+        List<Group> groupList = null;
+        try {
+            groupList = groupService.findAll();
+        } catch (SQLException e) {
+            model.addAttribute("errorMessage", "Failed to load data");
+        }
+        if (groupList != null){
+            model.addAttribute("groups", groupList);
+        }
+
+        return "students/new";
+    }
+
+    @PostMapping("/new")
+    public String postNewStudent(RedirectAttributes attributes,
+                                 @RequestParam Long group,
+                                 @RequestParam String firstName,
+                                 @RequestParam String lastName) {
+        if (firstName.length() > 0 && lastName.length() > 0 && group > 0) {
+            try {
+                Student student = new Student(firstName, lastName);
+                student.setGroupId(group);
+                studentService.save(student);
+            } catch (SQLException e) {
+                attributes.addAttribute("errorMessage", "failed to add entry");
+            }
+        }
+
+        return "redirect:/students";
+    }
+
+    @GetMapping("/edit/{id}")
+    public String editTeacher(Model model, @PathVariable long id) {
+        model.addAttribute("active", "students");
+        try {
+            model.addAttribute("student", studentService.findById(id));
+            model.addAttribute("groups", groupService.findAll());
+        } catch (SQLException e) {
+            model.addAttribute("errorMessage", "Failed to load data");
+        }
+
+        return "students/edit";
+    }
+
+    @PostMapping("/edit")
+    public String postEditTeacher(RedirectAttributes attributes,
+                                  @RequestParam String firstName,
+                                  @RequestParam String lastName,
+                                  @RequestParam Long group,
+                                  @RequestParam Long id) {
+        if (firstName.length() > 0 && lastName.length() > 0) {
+            Student student = new Student(firstName, lastName);
+            student.setGroupId(group);
+            student.setId(id);
+            try {
+                studentService.update(student);
+            } catch (SQLException e) {
+                attributes.addAttribute("errorMessage", "failed to update entry");
+            }
+        }
+        return "redirect:/students";
+    }
+
+    @GetMapping("/delete/{id}")
+    public String deleteTeacher(RedirectAttributes attributes, @PathVariable long id) {
+
+        try {
+            studentService.delete(id);
+        } catch (SQLException e) {
+            attributes.addAttribute("errorMessage", "failed to delete teacher");
+        }
+        return "redirect:/students";
     }
 
 }
