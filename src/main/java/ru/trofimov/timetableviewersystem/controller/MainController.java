c5ae96b786cbd72c3946cae3e2cd0f0064a28627
Trofimov <mikletrof@gmail.com>
2021-08-06 09:22:38 +0300

    added login page

--- src/main/java/ru/trofimov/timetableviewersystem/controller/MainController.java
+++ src/main/java/ru/trofimov/timetableviewersystem/controller/MainController.java
@@ -7,8 +7,20 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class MainController {
     @GetMapping("/")
-    public String showIndex( Model model){
+    public String showIndex(Model model){
         model.addAttribute("active","home");
         return "index";
+    }
+
+    @GetMapping("/home")
+    public String showIndex2(Model model){
+        model.addAttribute("active","home");
+        return "index";
+    }
+
+    @GetMapping("/login")
+    public String showLogin(Model model){
+        model.addAttribute("active","login");
+        return "login";
     }
 }
