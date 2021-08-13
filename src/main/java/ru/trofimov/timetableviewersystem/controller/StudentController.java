75ef1498be58e581ab03e2eb36a72c8ed8476b68
Trofimov <mikletrof@gmail.com>
2021-08-13 15:17:57 +0300

    updated to use UserService and UserGroupService

--- src/main/java/ru/trofimov/timetableviewersystem/controller/StudentController.java
+++ src/main/java/ru/trofimov/timetableviewersystem/controller/StudentController.java
@@ -4,93 +4,44 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
-import ru.trofimov.timetableviewersystem.model.Group;
 import ru.trofimov.timetableviewersystem.model.Student;
+import ru.trofimov.timetableviewersystem.model.User;
+import ru.trofimov.timetableviewersystem.model.UserGroup;
 import ru.trofimov.timetableviewersystem.service.GroupService;
-import ru.trofimov.timetableviewersystem.service.StudentService;
+import ru.trofimov.timetableviewersystem.service.UserGroupService;
 import ru.trofimov.timetableviewersystem.service.UserService;
 
 import java.sql.SQLException;
-import java.util.List;
 
 @Controller
 @RequestMapping("/students")
 public class StudentController {
-    private final StudentService studentService;
     private final GroupService groupService;
     private final UserService userService;
+    private final UserGroupService userGroupService;
 
-    public StudentController(StudentService studentService, GroupService groupService, UserService userService) {
-        this.studentService = studentService;
+    public StudentController(GroupService groupService, UserService userService, UserGroupService userGroupService) {
         this.groupService = groupService;
         this.userService = userService;
+        this.userGroupService = userGroupService;
     }
 
     @GetMapping()
     public String showAll(Model model) {
-        List<Group> groupList = null;
-        List<Student> studentList = null;
         model.addAttribute("active", "students");
         try {
-            System.out.println("userService.findAllStudent() = " + userService.findAllStudent());
-            studentList = studentService.findAll();
-            groupList = groupService.findAll();
+            model.addAttribute("students", userService.findAllStudent());
         } catch (SQLException e) {
             model.addAttribute("errorMessage", "Failed to load data");
-        }
-        if (studentList != null && groupList != null) {
-            List<Group> finalGroupList = groupList;
-            studentList.forEach(student -> student.setGroupName(
-                    finalGroupList.stream()
-                            .filter(group -> group.getId() == student.getGroupId())
-                            .findFirst().orElse(new Group("without group")).getGroupName()));
-//            );
-
-            model.addAttribute("students", studentList);
         }
         return "students/index";
     }
 
-    @GetMapping("/new")
-    public String newStudent(Model model) {
-        model.addAttribute("active", "students");
-        List<Group> groupList = null;
-        try {
-            groupList = groupService.findAll();
-        } catch (SQLException e) {
-            model.addAttribute("errorMessage", "Failed to load data");
-        }
-        if (groupList != null){
-            model.addAttribute("groups", groupList);
-        }
-
-        return "students/new";
-    }
-
-    @PostMapping("/new")
-    public String postNewStudent(RedirectAttributes attributes,
-                                 @RequestParam Long group,
-                                 @RequestParam String firstName,
-                                 @RequestParam String lastName) {
-        if (firstName.length() > 0 && lastName.length() > 0) {
-            if (group == 0) group = null;
-            try {
-                Student student = new Student(firstName, lastName);
-                student.setGroupId(group);
-                studentService.save(student);
-            } catch (SQLException e) {
-                attributes.addAttribute("errorMessage", "failed to add entry");
-            }
-        }
-
-        return "redirect:/students";
-    }
-
     @GetMapping("/edit/{id}")
-    public String editTeacher(Model model, @PathVariable long id) {
+    public String editStudent(Model model, @PathVariable long id) {
         model.addAttribute("active", "students");
         try {
-            model.addAttribute("student", studentService.findById(id));
+            model.addAttribute("student", userService.findStudentById(id));
             model.addAttribute("groups", groupService.findAll());
         } catch (SQLException e) {
             model.addAttribute("errorMessage", "Failed to load data");
@@ -106,12 +57,30 @@ public class StudentController {
                                   @RequestParam Long group,
                                   @RequestParam Long id) {
         if (firstName.length() > 0 && lastName.length() > 0) {
-            if (group == 0) group = null;
-            Student student = new Student(firstName, lastName);
-            student.setGroupId(group);
-            student.setId(id);
             try {
-                studentService.update(student);
+                Student student = userService.findStudentById(id);
+                System.out.println("Test: " + student.getGroupId() + " " + group);
+                if (!student.getGroupId().equals(group)) {
+                    if (group != 0) {
+                        if (student.getGroupId() == 0) {
+                            userGroupService.save(new UserGroup(id, group));
+                        } else {
+                            userGroupService.update(new UserGroup(id, group));
+                        }
+                    } else {
+                        userGroupService.deleteByUserId(id);
+                    }
+                }
+                if (!student.getFullName().equals(firstName + " " + lastName)) {
+                    userService.update(new User(
+                            id,
+                            firstName,
+                            lastName,
+                            student.getLogin(),
+                            student.getPassword(),
+                            student.getRoles()
+                    ));
+                }
             } catch (SQLException e) {
                 attributes.addAttribute("errorMessage", "failed to update entry");
             }
@@ -123,11 +92,10 @@ public class StudentController {
     public String deleteTeacher(RedirectAttributes attributes, @PathVariable long id) {
 
         try {
-            studentService.delete(id);
+            userService.delete(id);
         } catch (SQLException e) {
             attributes.addAttribute("errorMessage", "failed to delete students");
         }
         return "redirect:/students";
     }
-
 }
