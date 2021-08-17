67cc0ef1c9a7705b4dac970f1141e69ba5eef08c
Trofimov <mikletrof@gmail.com>
2021-08-17 14:58:04 +0300

    test ControllerAdvice

---
+++ src/main/java/ru/trofimov/timetableviewersystem/exception/ErrorObject.java
@@ -0,0 +1,34 @@
+package ru.trofimov.timetableviewersystem.exception;
+
+public class ErrorObject {
+
+    private int statusCode;
+
+    private String message;
+
+    private  long timestamp;
+
+    public int getStatusCode() {
+        return statusCode;
+    }
+
+    public void setStatusCode(int statusCode) {
+        this.statusCode = statusCode;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public long getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(long timestamp) {
+        this.timestamp = timestamp;
+    }
+}
---
+++ src/main/java/ru/trofimov/timetableviewersystem/exception/SQLException.java
@@ -0,0 +1,8 @@
+package ru.trofimov.timetableviewersystem.exception;
+
+public class SQLException extends RuntimeException{
+
+    public SQLException(String message) {
+        super(message);
+    }
+}
